d7c368254a53a8d517a64006fcaa83848ae102f4
Liming <shazi0415@qq.com>
2020-03-29 23:58:32 -0400

    preprocessing

--- load_data.py
+++ load_data.py
@@ -1,18 +1,41 @@
-import json
-from util import preprocess
+import os
+from util import make_stitched_data, QADataset
+import pickle
+from torch.utils.data import DataLoader
 
-with open('../data/quAIL/train_questions.json', 'rb') as f:
-  data = json.load(f)
 
-artical_list =  list(data['data'].keys())
+train_stitched_data_file_name = 'train_stitched_data.txt'
+dev_stitched_data_file_name = 'dev_stitched_data.txt'
 
-print(data['data']['u001']['questions']['u001_0']['answers'].keys())
+f_list = os.listdir()
+if train_stitched_data_file_name not in f_list:
+    make_stitched_data(file_name=train_stitched_data_file_name)
+if dev_stitched_data_file_name not in f_list:
+    make_stitched_data(file_name=dev_stitched_data_file_name)
 
-q = data['data']['u001']['questions']['u001_0']['question']
-ans = data['data']['u001']['questions']['u001_0']['answers']['0']
-print(q)
-res = preprocess(q)
-print(res)
+with open(train_stitched_data_file_name, 'rb') as f:
+    train_data = pickle.load(f)
+with open(dev_stitched_data_file_name, 'rb') as f:
+    dev_data = pickle.load(f)
+
+quiz_length_list = [len(quiz) for quiz,label in train_data]
+max_quiz_length = max(quiz_length_list)
+
+train_texts =[quiz for quiz,label in train_data]
+train_labels = [label for quiz,label in train_data]
+dev_texts =[quiz for quiz,label in dev_data]
+dev_labels = [label for quiz,label in dev_data]
+
+dataset_train = QADataset(train_texts,train_labels,max_len=max_quiz_length)
+dataset_dev = QADataset(dev_texts,dev_labels,vocab=dataset_train.vocab,labels_vocab=dataset_train.labels_vocab,max_len=max_quiz_length)
+
+dataloader_train = DataLoader(dataset_train,shuffle=True,batch_size=64)
+dataloader_dev = DataLoader(dataset_dev,shuffle=True,batch_size=64)
+
+print(dataset_dev[100])
+for batch in dataloader_dev:
+    print(batch)
+    break
 
 """
 The structure of quAIL training dataset:
